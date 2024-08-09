59b5c27b1189212eb6175f6b64c8935f2718c1de
Iyay59 <113583149+Iyay59@users.noreply.github.com>
2024-08-10 01:21:38 +0700

    Update Tools-BugBounty-installer.sh

--- Tools-BugBounty-installer.sh
+++ Tools-BugBounty-installer.sh
@@ -18,11 +18,6 @@ echo -e "\e[32mDone!";
 sleep 1.5
 clear;
 
-#---------Install essential packets
-echo -e "\e[93m\e[1m----> Installing essential and useful packets-tools";
-apt-get install -y git rename findutils terminator chromium-browser tmux
-echo -e "\e[32mDone!";
-sleep 1.5
 
 #---------Generic OS
 #Python, ruby and some packages
@@ -31,15 +26,6 @@ apt-get install -y python python-pip python3 python3-pip python-dnspython python
 echo -e "\e[32mDone!";
 sleep 1.5
 
-#---------Install Golang
-echo -e "\e[93m\e[1m----> Golang environment installation in progress ...";
-cd /tmp && wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz > /dev/null 2>&1 && tar xvf go1.21.6.linux-amd64.tar.gz > /dev/null 2>&1;
-mv go /usr/local
-export GOROOT=/usr/local/go && export GOPATH=$HOME/go && export PATH=$GOPATH/bin:$GOROOT/bin:$PATH;
-echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile && echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile	&& echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile;
-source ~/.bash_profile
-echo -e "\e[32mGolang environment installation is done !"; echo "";
-sleep 1.5
 
 #---------Create a Tools folder in Home ~/
 echo -e "\e[93m\e[1m----> Create a Tools folder";
@@ -51,55 +37,55 @@ sleep 1.5
 #---------Install Network scanner
 #Nmap
 echo -e "\e[93m\e[1m----> Installing nmap";
-apt-get install -y nmap > /dev/null 2>&1;
+sudo apt install -y nmap;
 echo -e "\e[32mDone! Nmap installed.";
 sleep 1.5
 #Masscan
 echo -e "\e[93m\e[1m----> Installing Masscan";
-git clone https://github.com/robertdavidgraham/masscan > /dev/null 2>&1 && cd masscan && make > /dev/null 2>&1 && make install > /dev/null 2>&1 && mv bin/masscan /usr/local/bin/;
+git clone https://github.com/robertdavidgraham/masscan > && cd masscan && make > && make install > && mv bin/masscan /usr/local/bin/;
 echo -e "\e[32mDone! Masscan installed."; echo "";
 sleep 1.5
 #Naabu
 echo -e "\e[93m\e[1m----> Installing Naabu";
-go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu > /dev/null 2>&1 && ln -s ~/go/bin/naabu /usr/local/bin/;
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest > && ln -s ~/go/bin/naabu /usr/local/bin/;
 echo -e "\e[32mDone! Naabu installed."; echo "";
 sleep 1.5
 
 #---------Install subdomain enumeration and DNS Resolver
 #dnsutils
-apt-get install -y dnsutils > /dev/null 2>&1;
+sudo apt install -y dnsutils ;
 sleep 1.5
 #Massdns
 echo -e "\e[93m\e[1m----> Installing massdns";
-git clone https://github.com/blechschmidt/massdns.git > /dev/null 2>&1;
+git clone https://github.com/blechschmidt/massdns.git ;
 cd ~/tools/massdns
-make > /dev/null 2>&1;
+make > ;
 cd ~/tools/
 echo -e "\e[32mDone! Massdns installed."; echo "";
 sleep 1.5
 #Subfinder
 echo -e "\e[93m\e[1m----> Installing Subfinder";
-go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder > /dev/null 2>&1 && ln -s ~/go/bin/subfinder /usr/local/bin/;
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest > && ln -s ~/go/bin/subfinder /usr/local/bin/;
 echo -e "\e[32mDone! Subfinder installed."; echo "";
 sleep 1.5
 #Knock
 echo -e "\e[93m\e[1m----> Installing Knock";
-git clone https://github.com/guelfoweb/knock.git > /dev/null 2>&1;
+git clone https://github.com/guelfoweb/knock.git ;
 echo -e "\e[32mDone! Knock installed."; echo "";
 sleep 1.5
 #Lazyrecon
 echo -e "\e[93m\e[1m----> Installing LazyRecon";
-git clone https://github.com/nahamsec/lazyrecon.git > /dev/null 2>&1;
+git clone https://github.com/nahamsec/lazyrecon.git ;
 echo -e "\e[32mDone! LazyRecon installed."; echo "";
 sleep 1.5
 #Github-subdomains
 echo -e "\e[93m\e[1m----> Installing Github-subdomains";
-go get -u github.com/gwen001/github-subdomains > /dev/null 2>&1 && ln -s ~/go/bin/github-subdomains /usr/local/bin/;
+go install github.com/gwen001/github-subdomains@latest > && ln -s ~/go/bin/github-subdomains /usr/local/bin/;
 echo -e "\e[32mDone! Github-subdomains installed."; echo "";
 sleep 1.5
 #Sublist3r
 echo -e "\e[93m\e[1m----> Installing Sublist3r";
-git clone https://github.com/aboul3la/Sublist3r.git > /dev/null 2>&1;
+git clone https://github.com/aboul3la/Sublist3r.git > ;
 cd Sublist3r*
 pip install -r requirements.txt
 cd ~/tools/
@@ -107,36 +93,36 @@ echo -e "\e[32mDone! Sublist3r installed."; echo "";
 sleep 1.5
 #Crtndstry
 echo -e "\e[93m\e[1m----> Installing Crtndstry";
-git clone https://github.com/nahamsec/crtndstry.git > /dev/null 2>&1;
+git clone https://github.com/nahamsec/crtndstry.git > ;
 echo -e "\e[32mDone! Crtndstry installed."; echo "";
 sleep 1.5
 #Assetfinder
 echo -e "\e[93m\e[1m----> Installing Assentfinder";
-go get -u github.com/tomnomnom/assetfinder > /dev/null 2>&1 && ln -s ~/go/bin/assetfinder /usr/local/bin/;
+go install github.com/tomnomnom/assetfinder@latest > && ln -s ~/go/bin/assetfinder /usr/local/bin/;
 echo -e "\e[32mDone! Assetfinder installed."; echo "";
 sleep 1.5
 #dnsx
 echo -e "\e[93m\e[1m----> Installing Dnsx";
-go get -v github.com/projectdiscovery/dnsx/cmd/dnsx > /dev/null 2>&1 && ln -s ~/go/bin/dnsx /usr/bin/;
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest > && ln -s ~/go/bin/dnsx /usr/bin/;
 echo -e "\e[32mDone! Dnsx installed."; echo "";
 sleep 1.5
 #dnsgen
 echo -e "\e[93m\e[1m----> Installing Dnsgen";
-pip3 install dnsgen > /dev/null 2>&1;
+pip3 install dnsgen ;
 echo -e "\e[32mDone! Dnsgen installed."; echo "";
 sleep 1.5
 
 #---------Install subdomain takeovers
 #SubOver
 echo -e "\e[93m\e[1m----> Installing SubOver";
-go get -v github.com/Ice3man543/SubOver > /dev/null 2>&1 && ln -s ~/go/bin/subover /usr/bin/;
+go install -v github.com/Ice3man543/SubOver@latest > && ln -s ~/go/bin/subover /usr/bin/;
 echo -e "\e[32mDone! SubOver installed."; echo "";
 sleep 1.5
 
 #---------Install Fuzzing Tools
 #Dirsearch
 echo -e "\e[93m\e[1m----> Installing dirsearch";
-git clone https://github.com/maurosoria/dirsearch.git > /dev/null 2>&1;
+git clone https://github.com/maurosoria/dirsearch.git > ;
 cd dirsearch*
 pip3 install -r requirements.txt
 cd ~/tools/
@@ -144,14 +130,14 @@ echo -e "\e[32mDone! Dirsearch installed.";
 sleep 1.5
 #ffuf
 echo -e "\e[93m\e[1m----> Installing ffuf";
-go get -u github.com/ffuf/ffuf > /dev/null 2>&1 && ln -s ~/go/bin/ffuf /usr/local/bin/;
+go install -u github.com/ffuf/ffuf@latest > && ln -s ~/go/bin/ffuf /usr/local/bin/;
 echo -e "\e[32mDone! Ffuf installed.";
 sleep 1.5
 
 #---------Wordlists
 echo -e "\e[93m\e[1m----> Downloading Seclists";
 cd ~/tools/
-git clone https://github.com/danielmiessler/SecLists.git > /dev/null 2>&1;
+git clone https://github.com/danielmiessler/SecLists.git > ;
 cd ~/tools/SecLists/Discovery/DNS/
 #This file must be cleaned, as it breaks massdns
 cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
@@ -163,32 +149,32 @@ sleep 1.5
 #---------Scanner CMS
 #WPScan
 echo -e "\e[93m\e[1m----> Installing wpscan";
-gem install wpscan > /dev/null 2>&1;
+gem install wpscan > ;
 echo -e "\e[32mDone! wpscan installed."; echo "";
 sleep 1.5
 #Droopescan
 echo -e "\e[93m\e[1m----> Installing Droopescan";
-pip install droopescan > /dev/null 2>&1;
+pip install droopescan > ;
 echo -e "\e[32mDone! Droopescan installed."; echo "";
 sleep 1.5
 
 #---------Vuln SQL
 #SQLmap
 echo -e "\e[93m\e[1m----> Installing sqlmap";
-apt-get install -y sqlmap > /dev/null 2>&1;
+sudo apt install -y sqlmap > ;
 echo -e "\e[32mDone! Sqlmap installed."; echo "";
 sleep 1.5
 #NoSQLMap
 echo -e ${BLUE}"[VULNERABILITY - SQL Injection]" ${RED}"NoSQLMap installation in progress ...";
-git clone https://github.com/codingo/NoSQLMap.git > /dev/null 2>&1;
+git clone https://github.com/codingo/NoSQLMap.git > ;
 cd NoSQLMap
-python setup.py install > /dev/null 2>&1;
+python setup.py install > ;
 cd ~/tools/
 echo -e "\e[32mDone! NoSqlmap installed."; echo "";
 sleep 1.5
 #Jeeves
 echo -e "\e[93m\e[1m----> Installing Jeeves";
-go get -u github.com/ferreiraklet/Jeeves > /dev/null 2>&1 && ln -s ~/go/bin/jeeves /usr/local/bin/;
+go install -u github.com/ferreiraklet/Jeeves@latest > && ln -s ~/go/bin/jeeves /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Jeeves installed."; echo "";
 sleep 1.5
@@ -197,23 +183,23 @@ sleep 1.5
 #---------Enumeration JavaScript
 #LinkFinder
 echo -e "\e[93m\e[1m----> Installing LinkFinder";
-git clone https://github.com/GerbenJavado/LinkFinder.git > /dev/null 2>&1;
+git clone https://github.com/GerbenJavado/LinkFinder.git > ;
 cd LinkFinder
-pip3 install -r requirements.txt > /dev/null 2>&1 && python3 setup.py install > /dev/null 2>&1;
+pip3 install -r requirements.txt > && python3 setup.py install > ;
 cd ~/tools/
 echo -e "\e[32mDone! LinkFinder installed."; echo "";
 sleep 1.5
 #SecretFinder
 echo -e "\e[93m\e[1m----> Installing SecretFinder";
-git clone https://github.com/m4ll0k/SecretFinder.git > /dev/null 2>&1;
+git clone https://github.com/m4ll0k/SecretFinder.git > ;
 cd SecretFinder
-pip3 install -r requirements.txt > /dev/null 2>&1;
+pip3 install -r requirements.txt > ;
 cd ~/tools/
 echo -e "\e[32mDone! SecretFinder installed."; echo "";
 sleep 1.5
 #JSParser
 echo -e "\e[93m\e[1m----> Installing JSParser";
-git clone https://github.com/nahamsec/JSParser.git > /dev/null 2>&1;
+git clone https://github.com/nahamsec/JSParser.git > ;
 cd JSParser*
 sudo python setup.py install
 cd ~/tools/
@@ -223,7 +209,7 @@ sleep 1.5
 #---------Visual Recon
 #aquatone
 echo -e "\e[93m\e[1m----> Installing Aquatone";
-cd /tmp && wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip > /dev/null 2>&1 && unzip aquatone_linux_amd64_1.7.0.zip > /dev/null 2>&1;
+cd /tmp && wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_arm64_1.7.0.zip > && unzip aquatone_linux_arm64_1.7.0.zip > ;
 mv aquatone /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Aquatone installed."; echo "";
@@ -232,19 +218,19 @@ sleep 1.5
 #---------Crawling Web
 #Gospider
 echo -e "\e[93m\e[1m----> Installing Gospider";
-go install github.com/jaeles-project/gospider@latest > /dev/null 2>&1 && ln -s ~/go/bin/gospider /usr/local/bin/;
+go install github.com/jaeles-project/gospider@latest > && ln -s ~/go/bin/gospider /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Gospider installed."; echo "";
 sleep 1.5
 #Hakrawler
 echo -e "\e[93m\e[1m----> Installing Hakrawler";
-go install github.com/hakluke/hakrawler@latest > /dev/null 2>&1 && ln -s ~/go/bin/hakrawler /usr/local/bin/;
+go install github.com/hakluke/hakrawler@latest > && ln -s ~/go/bin/hakrawler /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Hakrawler installed."; echo "";
 sleep 1.5
 #Katana
 echo -e "\e[93m\e[1m----> Installing Katana";
-go install github.com/projectdiscovery/katana/cmd/katana@latest > /dev/null 2>&1 && ln -s ~/go/bin/katana /usr/local/bin/;
+go install github.com/projectdiscovery/katana/cmd/katana@latest > && ln -s ~/go/bin/katana /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Katana installed."; echo "";
 sleep 1.5
@@ -252,31 +238,31 @@ sleep 1.5
 #---------Vuln XSS
 #XSStrike
 echo -e "\e[93m\e[1m----> Installing XSStrike";
-git clone https://github.com/s0md3v/XSStrike > /dev/null 2>&1 && cd XSStrike && pip3 install -r requirements.txt > /dev/null 2>&1;
+git clone https://github.com/s0md3v/XSStrike > && cd XSStrike && pip3 install -r requirements.txt > ;
 cd ~/tools/
 echo -e "\e[32mDone! XSStrike installed."; echo "";
 sleep 1.5
 #XSS-Loader
 echo -e "\e[93m\e[1m----> Installing XSS-Loader";
-git clone https://github.com/capture0x/XSS-LOADER/ > /dev/null 2>&1 && cd XSS-LOADER && pip3 install -r requirements.txt > /dev/null 2>&1;
+git clone https://github.com/capture0x/XSS-LOADER/ > && cd XSS-LOADER && pip3 install -r requirements.txt > ;
 cd ~/tools/
 echo -e "\e[32mDone! XSS-Loader installed."; echo "";
 sleep 1.5
 #Freq
 echo -e "\e[93m\e[1m----> Installing Freq";
-go install github.com/takshal/freq@latest > /dev/null 2>&1 && ln -s ~/go/bin/freq /usr/local/bin/;
+go install github.com/takshal/freq@latest > && ln -s ~/go/bin/freq /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Freq installed."; echo "";
 sleep 1.5
 #Gxss
 echo -e "\e[93m\e[1m----> Installing Gxss";
-go install github.com/KathanP19/Gxss@latest > /dev/null 2>&1 && ln -s ~/go/bin/Gxss /usr/local/bin/;
+go install github.com/KathanP19/Gxss@latest > && ln -s ~/go/bin/Gxss /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Gxss installed."; echo "";
 sleep 1.5
 #Dalfox
 echo -e "\e[93m\e[1m----> Installing Dalfox";
-go install github.com/hahwul/dalfox/v2@latest > /dev/null 2>&1 && ln -s ~/go/bin/dalfox /usr/local/bin/;
+go install github.com/hahwul/dalfox/v2@latest > && ln -s ~/go/bin/dalfox /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Dalfox installed."; echo "";
 sleep 1.5
@@ -284,13 +270,13 @@ sleep 1.5
 #---------Vuln SSRF
 #SSRFmap
 echo -e "\e[93m\e[1m----> Installing SSRFmap";
-git clone https://github.com/swisskyrepo/SSRFmap > /dev/null 2>&1 && cd SSRFmap && pip3 install -r requirements.txt > /dev/null 2>&1;
+git clone https://github.com/swisskyrepo/SSRFmap > && cd SSRFmap && pip3 install -r requirements.txt > ;
 cd ~/tools/
 echo -e "\e[32mDone! SSRFmap installed."; echo "";
 sleep 1.5
 #Gopherus
 echo -e "\e[93m\e[1m----> Installing Gopherus";
-git clone https://github.com/tarunkant/Gopherus.git > /dev/null 2>&1 && cd Gopherus && chmod +x install.sh && ./install.sh > /dev/null 2>&1;
+git clone https://github.com/tarunkant/Gopherus.git > && cd Gopherus && chmod +x install.sh && ./install.sh > ;
 cd ~/tools/
 echo -e "\e[32mDone! Gopherus installed."; echo "";
 sleep 1.5
@@ -298,7 +284,7 @@ sleep 1.5
 #---------Vuln Scan
 #Nuclei
 echo -e "\e[93m\e[1m----> Installing Nuclei";
-go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei > /dev/null 2>&1 && ln -s ~/go/bin/nuclei /usr/local/bin/;
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest > && ln -s ~/go/bin/nuclei /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Nuclei installed."; echo "";
 sleep 1.5
@@ -306,7 +292,7 @@ sleep 1.5
 #---------Virtual Host Discovery
 #VirtualHostDiscovery
 echo -e "\e[93m\e[1m----> Installing VirtualHostDiscovery";
-git clone https://github.com/jobertabma/virtual-host-discovery.git  > /dev/null 2>&1;
+git clone https://github.com/jobertabma/virtual-host-discovery.git  > ;
 cd ~/tools/
 echo -e "\e[32mDone! VirtualHostDiscovery installed."; echo "";
 sleep 1.5
@@ -314,70 +300,70 @@ sleep 1.5
 #---------Useful Tools
 #anew
 echo -e "\e[93m\e[1m----> Installing Anew";
-go install -v github.com/tomnomnom/anew@latest > /dev/null 2>&1 && ln -s ~/go/bin/anew /usr/local/bin/;
+go install -v github.com/tomnomnom/anew@latest > && ln -s ~/go/bin/anew /usr/local/bin/;
 echo -e "\e[32mDone! Dnsgen installed."; echo "";
 sleep 1.5
 #unew
 echo -e "\e[93m\e[1m----> Installing Unew";
-go get -u github.com/dwisiswant0/unew > /dev/null 2>&1 && ln -s ~/go/bin/unew /usr/local/bin/;
+go install -u github.com/dwisiswant0/unew@latest > && ln -s ~/go/bin/unew /usr/local/bin/;
 echo -e "\e[32mDone! Dnsgen installed."; echo "";
 sleep 1.5
 #gf
 echo -e "\e[93m\e[1m----> Installing gf";
-go get -u github.com/tomnomnom/gf > /dev/null 2>&1 && ln -s ~/go/bin/gf /usr/local/bin/;
+go install -u github.com/tomnomnom/gf@latest > && ln -s ~/go/bin/gf /usr/local/bin/;
 echo -e "\e[32mDone! Gf installed."; echo "";
 sleep 1.5
 #Http Probe
 echo -e "\e[93m\e[1m----> Installing httprobe";
-go get github.com/tomnomnom/httprobe > /dev/null 2>&1 && ln -s ~/go/bin/httprobe /usr/local/bin/;
+go install github.com/tomnomnom/httprobe > && ln -s ~/go/bin/httprobe /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Httprobe installed."; echo "";
 sleep 1.5
 #httpx
 echo -e "\e[93m\e[1m----> Installing httpx";
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest > /dev/null 2>&1 && ln -s ~/go/bin/httpx /usr/local/bin/;
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest > && ln -s ~/go/bin/httpx /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Httpx installed."; echo "";
 sleep 1.5
 #waybackurls
 echo -e "\e[93m\e[1m----> Installing waybackurls";
-go install github.com/tomnomnom/waybackurls@latest > /dev/null 2>&1 && ln -s ~/go/bin/waybackurls /usr/local/bin/;
+go install github.com/tomnomnom/waybackurls@latest > && ln -s ~/go/bin/waybackurls /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Waybackurls installed."; echo "";
 sleep 1.5
 #Arjun
 echo -e "\e[93m\e[1m----> Installing arjun";
-pip3 install arjun > /dev/null 2>&1;
+pip3 install arjun > ;
 echo -e "\e[32mDone! Arjun installed."; echo "";
 sleep 1.5
 #Gau
 echo -e "\e[93m\e[1m----> Installing gau";
-go install github.com/lc/gau/v2/cmd/gau@latest > /dev/null 2>&1 && ln -s ~/go/bin/gau /usr/local/bin/;
+go install github.com/lc/gau/v2/cmd/gau@latest > && ln -s ~/go/bin/gau /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Gau installed."; echo "";
 sleep 1.5
 #Gauplus
 echo -e "\e[93m\e[1m----> Installing gauplus";
-go install github.com/bp0lr/gauplus@latest  > /dev/null 2>&1 && ln -s ~/go/bin/gauplus /usr/local/bin/;
+go install github.com/bp0lr/gauplus@latest  > && ln -s ~/go/bin/gauplus /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Gauplus installed."; echo "";
 sleep 1.5
 #Uro
 echo -e "\e[93m\e[1m----> Installing uro";
-pip3 install uro > /dev/null;
+pip3 install uro > ;
 cd ~/tools/
 echo -e "\e[32mDone! Uro installed."; echo "";
 sleep 1.5
 #QSreplace
 echo -e "\e[93m\e[1m----> Installing qsreplace";
-go get -v github.com/tomnomnom/qsreplace > /dev/null 2>&1 && ln -s ~/go/bin/qsreplace /usr/local/bin/;
+go install -v github.com/tomnomnom/qsreplace@latest > && ln -s ~/go/bin/qsreplace /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Qsreplace installed."; echo "";
 sleep 1.5
 sleep 1.5
 #SocialHunter
 echo -e "\e[93m\e[1m----> Installing Socialhunter";
-go install github.com/utkusen/socialhunter@latest > /dev/null 2>&1 && ln -s ~/go/bin/socialhunter /usr/local/bin/;
+go install github.com/utkusen/socialhunter@latest > && ln -s ~/go/bin/socialhunter /usr/local/bin/;
 cd ~/tools/
 echo -e "\e[32mDone! Socialhunter installed."; echo "";
 sleep 1.5
